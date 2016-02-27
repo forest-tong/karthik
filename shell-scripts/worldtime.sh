e02695db7c357d8c4b82ef6cd9cfba97a92c724a
Forest Tong <ftong@mit.edu>
2016-02-27 11:14:34 -0500

    Added basic worldtime script and colors

---
+++ shell-scripts/worldtime.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# Print the current time in specified time zones.
+# Helpful link: http://unix.stackexchange.com/questions/48101/how-can-i-have-date-output-the-time-from-a-different-timezone
+#
+
+
+echo "\033[91mWorld Time"
+
+# TZONES refers to system time zones,
+# displayed with the names in TZONE_NAMES.
+# The corresponding cities in TZONES and TZONE_NAMES
+# should have the same time zone.
+# See /usr/share/zoneinfo/ for more names.
+TZONES=(
+  'US/Los_Angeles'
+  'US/Chicago'
+)
+TZONE_NAMES=(
+  'Seattle'
+  'Austin'
+)
+
+# Show the time from the specified input time zone in the specified output
+# time zone
+for (( i=0; i<${#TZONES[@]}; i++ )); do
+  TZONE=${TZONES[$i]}
+  TZONE_NAME=${TZONE_NAMES[$i]}
+  TZONE_DATE=`TZ=$TZONE date`
+  echo "$TZONE_NAME: $TZONE_DATE"
+done
